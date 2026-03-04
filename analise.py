e82a6463f23dab2dfa5d7b3b9e3fe875f21844ee
lucas_palmeiras <lucasmassoni02@gmail.com>
2026-03-04 17:22:25 -0300

    Dois novos calculos efetuados: trafego x hora para um dia especifico, e média do trafego por hora com desvio padrão

--- analise.py
+++ analise.py
@@ -19,10 +19,33 @@ plt.show()
 
 '''
 ### FORMA NUMERO 2 DE TRAZER A ESTATISTICA NUMERO DE VEICULOS X CONDIÇÃO CLIMATICA (MELHOR)###
+'''
 veiculos_clima = df.groupby('weather_main')['traffic_volume'].agg(['mean','sum','max','min']) #groupby funciona como no SQL, e .agg agrega uma série de operações possiveis
 
 plt.bar(veiculos_clima.index,veiculos_clima['max'])
 plt.bar(veiculos_clima.index,veiculos_clima['min'])
 plt.show()
+'''
 
+print(df)
+### TENTATIVA NUMERO 2: evolução do trafego por hora ###
 
+df['date_time'] = pd.to_datetime(df['date_time'])
+df['hora'] = df['date_time'].dt.hour
+df['dia'] = df['date_time'].dt.day_of_week
+df['mes'] = df['date_time'].dt.month
+df['ano'] = df['date_time'].dt.year
+df['ano'] = df['date_time'].dt.date
+
+df_um_dia = df[df['date_time'].dt.date == pd.to_datetime('2012-10-03').date()]
+'''
+plt.plot(df_um_dia['hora'],df_um_dia['traffic_volume'])
+plt.show()
+'''
+### TENTATIVA 3: FAZER A MEDIA PARA TODOS OS DIAS DA EVOLUÇÃO DO TRAFEGO POR HORA ###
+df_media = df.groupby(df['date_time'].dt.hour)['traffic_volume'].agg(['mean','std'])
+
+print(df_media)
+
+plt.errorbar(df_media.index, df_media['mean'],df_media['std'])
+plt.show()
